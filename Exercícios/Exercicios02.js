c03169431695feb17ced6c106636b2bad87650f0
bryansoaresschmidt <bryansoaresschmidt@gmail.com>
2024-06-22 23:29:45 -0300

    aula 18

--- Exercícios/Exercicios02.js
+++ Exercícios/Exercicios02.js
@@ -1348,3 +1348,7 @@ if (false) {
   console.log(retornarNovoObjeto(dados))
   
 }
+
+/*Exercício 43. Dado dois objetos, obj1 e obj2, escreva uma função que crie um novo objeto
+combinando as propriedades de ambos, onde as propriedades de obj2 têm precedência
+sobre as do obj1 em caso de conflitos.*/
