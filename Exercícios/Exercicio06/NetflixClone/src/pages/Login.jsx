81d53d67e4b451a00153737af8015e330a1d3b63
bryansoaresschmidt <bryansoaresschmidt@gmail.com>
2024-08-31 16:52:40 -0300

    31/08

---
+++ Exercícios/Exercicio06/NetflixClone/src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import FormComp from "./component/FormComp"
+import Header from "./component/Header"
+import Footer from './component/Footer'
+
+function Login() {
+
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <FormComp />
+      <Footer />
+    </>
+  )
+}
+
+export default Login
